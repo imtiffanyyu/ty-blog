a2d26cc4473b4891f64c1d2e43375d0c596db7fe
Tiffany Yu <tiffanyayu@gmail.com>
2016-05-30 18:09:47 +0200

    Added in ability to add new post

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 // connect to the database
-var sequelize = new Sequelize('tiffany', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+var sequelize = new Sequelize('tiffany', 'tiffany', null, {
 	host: 'localhost',
 	dialect: 'postgres',
 	define: {
@@ -19,7 +19,19 @@ var User = sequelize.define('user', {
 	password: Sequelize.STRING
 });
 
+//create table called posts
+var userPost = sequelize.define('post', {
+	title: Sequelize.STRING,
+	body: Sequelize.TEXT
+});
+
+// assigns user to posts
+User.hasMany(userPost);
+userPost.belongsTo(User);
+
 var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(session({ 
 	secret: 'oh wow very secret much security',
@@ -38,16 +50,49 @@ app.get('/', function (request, response) {
 	});
 });
 
+
+// display a form to create a new user
+app.get('/new', function (request, response) {
+	response.render('new');
+});
+
+// create the new user
+app.post('/users', bodyParser.urlencoded({extended: true}), function (request, response) {
+	User.create({
+		name: request.body.name,
+		email: request.body.email,
+		password: request.body.password
+	}).then(function (user) {
+		response.redirect('/profile');
+	});
+});
+
+
+//creates new post
+app.post('/blog', bodyParser.urlencoded({extended: true}), function (request, response) {
+	userPost.create({
+		title: request.body.title,
+		body: request.body.body,
+		userId: request.session.userId
+	}).then(function (blog) {
+		response.redirect('/profile');
+	})
+});
+
 app.get('/profile', function (request, response) {
 	var user = request.session.user;
+	var blog = userPost;
 	if (user === undefined) {
-		response.redirect('/?message=' + encodeURIComponent("Please log in to view your profile.")); // makes the string URL friendly
+		response.redirect('/?message=' + encodeURIComponent("Please log in to access the blog.")); // makes the string URL friendly
 	} else {
 		response.render('profile', {
-			user: user
+			user: user,
+			blog: blog
 		});
 	}
 });
+
+
 
 app.post('/login', bodyParser.urlencoded({extended: true}), function (request, response) {
 	if(request.body.email.length === 0) {
@@ -84,6 +129,8 @@ app.get('/logout', function (request, response) {
 		response.redirect('/?message=' + encodeURIComponent("Successfully logged out."));
 	})
 });
+
+
 
 sequelize.sync().then(function () {
 	var server = app.listen(3000, function () {
