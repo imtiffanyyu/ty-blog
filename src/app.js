fd1c470d9151b6a65ce56a047a0a74a761f4cd19
Tiffany Yu <tiffanyayu@gmail.com>
2016-05-30 16:25:55 +0200

    Added login / logout functionality

---
+++ src/app.js
@@ -0,0 +1,93 @@
+var Sequelize = require('sequelize');
+var express = require('express');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+// connect to the database
+var sequelize = new Sequelize('tiffany', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+	host: 'localhost',
+	dialect: 'postgres',
+	define: {
+		timestamps: false
+	}
+});
+
+//create table called users
+var User = sequelize.define('user', {
+	name: Sequelize.STRING,
+	email: Sequelize.STRING,
+	password: Sequelize.STRING
+});
+
+var app = express();
+
+app.use(session({ 
+	secret: 'oh wow very secret much security',
+	resave: true,
+	saveUninitialized: false
+}));
+
+app.set('views', './src/views');
+app.set('view engine', 'jade');
+
+// initial login to create a session for the user
+app.get('/', function (request, response) {
+	response.render('index', {
+		message: request.query.message, 
+		user: request.session.user 
+	});
+});
+
+app.get('/profile', function (request, response) {
+	var user = request.session.user;
+	if (user === undefined) {
+		response.redirect('/?message=' + encodeURIComponent("Please log in to view your profile.")); // makes the string URL friendly
+	} else {
+		response.render('profile', {
+			user: user
+		});
+	}
+});
+
+app.post('/login', bodyParser.urlencoded({extended: true}), function (request, response) {
+	if(request.body.email.length === 0) {
+		response.redirect('/?message=' + encodeURIComponent("Please fill out your email address.")); 
+		return;
+	}
+
+	if(request.body.password.length === 0) {
+		response.redirect('/?message=' + encodeURIComponent("Please fill out your password."));
+		return;
+	}
+
+	User.findOne({
+		where: {
+			email: request.body.email
+		}
+	}).then(function (user) {
+		if (user !== null && request.body.password === user.password) { // from the database
+			request.session.user = user;
+			response.redirect('/profile');
+		} else {
+			response.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
+		}
+	}, function (error) {
+		response.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
+	});
+});
+
+app.get('/logout', function (request, response) {
+	request.session.destroy(function(error) {
+		if(error) {
+			throw error;
+		}
+		response.redirect('/?message=' + encodeURIComponent("Successfully logged out."));
+	})
+});
+
+sequelize.sync().then(function () {
+	var server = app.listen(3000, function () {
+		console.log('Example app listening on port: ' + server.address().port);
+	});
+});
+
